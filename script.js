e9fe5e1f0cad6c2900b106b91b6ec26e36c1cf15
Priyankasawant018 <priyankasawant9675@gmail.com>
2024-07-14 12:04:17 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example: Display a message when form is submitted
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Message sent successfully!');
+});
